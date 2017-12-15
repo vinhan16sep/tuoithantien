3c1f850ddf152ecceff50d432c462980e144f72d
minhtruongmato <truong.do@matocreative.vn>
2017-12-15 17:30:28 +0700

    [in progress] ttt-34

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -9,8 +9,18 @@ class Dashboard extends Admin_Controller {
     }
 
     public function index(){
+        $this->load->model('theme_model');
+        $this->data['themes'] = $this->theme_model->fetch_all();
+
         $this->data['count'] = $this->contact_model->count_survey();
+
         $this->render('admin/dashboard_view');
+    }
+
+    public function edit_theme(){
+        $this->load->model('theme_model');
+        $id = $this->input->get('id');
+        echo $id;
     }
 
     public function fetch_survey(){
