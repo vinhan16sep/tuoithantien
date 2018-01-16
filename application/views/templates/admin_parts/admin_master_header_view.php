68ef9e20035c3672071c42d245ab8acf926e43ed
hungluong249 <hung.luong@matocreative.vn>
2018-01-16 09:31:26 +0700

    update favicon

--- application/views/templates/admin_parts/admin_master_header_view.php
+++ application/views/templates/admin_parts/admin_master_header_view.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php echo $page_title; ?></title>
+  <link rel="shortcut icon" type="image/png" href="<?php echo site_url('assets/public/img/favicon.png'); ?>"/>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
